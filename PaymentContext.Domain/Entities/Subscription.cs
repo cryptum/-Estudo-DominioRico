efba6863bf157640370e83d21a25b1a0e70e3538
Danilo Bernardes <danilobernardes@tokenlab.com.br>
2021-02-18 17:23:51 -0300

    Mudança do .NET 5 -> .NET Core 3.1 por compatibilidade no Ubuntu. Adicionado Entidades: Payment, Student, Subscription

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace PaymentContext.Domain.Entities
 {
     public class Subscription
@@ -5,6 +7,7 @@ namespace PaymentContext.Domain.Entities
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
         public DateTime? ExpireDate { get; set; }
-        public List<Subscription> { get; set; }
+        public bool Active { get; set; }
+        public List<Payment> Payments { get; set; }
     }
 }
